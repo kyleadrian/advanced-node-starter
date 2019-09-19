6625e9aa56a86920292992a11a868d1ac55e440c
Kyle Wiltshire <Kyle@Kyles-Air.lan1>
2019-09-19 14:17:21 -0400

    initial commite

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://kyleadrian:DPfcXMhKr1Eo9lpq@anjs-1-shard-00-00-etc6m.mongodb.net:27017,anjs-1-shard-00-01-etc6m.mongodb.net:27017,anjs-1-shard-00-02-etc6m.mongodb.net:27017/test?ssl=true&replicaSet=ANJS-1-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
+
